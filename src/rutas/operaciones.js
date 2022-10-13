1b699d1e784bb4db9b61674be54c4f917f5191b5
kevinderitis <kevin.deritis@hotmail.com>
2022-10-12 22:23:39 -0300

    Servidor en capas - Middleware auth

---
+++ src/rutas/operaciones.js
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import controlador from '../controlador/operaciones.js'
+
+import { auth } from './middleware/auth.js'
+
+const router = Router()
+
+router.get('/suma', controlador.suma)
+
+router.get('/resta', controlador.resta)
+
+router.get('/mult', controlador.mult)
+
+router.get('/div', controlador.div)
+
+router.get('/listar', auth, controlador.listar)
+
+router.get('/info', auth, controlador.info)
+
+export default router
